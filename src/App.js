d132e209aacc901c3ab118e48c80dfa92cbc7a1b
antoniluthfi <luthfiantoni027@gmail.com>
2021-04-01 10:24:58 +0700

    lognin state redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,21 +8,45 @@ import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import RegisterComplete from './pages/auth/RegisterComplete';
 
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+
 import Header from './components/nav/Header';
 
 const  App = () => {
-  return (
-    <>
-      <ToastContainer hideProgressBar />
-      <Header/>
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/login' exact component={Login} />
-        <Route path='/register' exact component={Register} />
-        <Route path='/register/complete' exact component={RegisterComplete} />
-      </Switch>
-    </>
-  );
+    const dispatch = useDispatch();
+
+    // check firebase auth state
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(async user => {
+            if(user) {
+                const idTokenResult = await user.getIdTokenResult();
+
+                dispatch({
+                    type: 'LOGGED_IN_USER',
+                    payload: {
+                        email: user.email,
+                        token: idTokenResult.token
+                    }
+                })
+            }
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <>
+            <ToastContainer hideProgressBar />
+            <Header/>
+            <Switch>
+                <Route path='/' exact component={Home} />
+                <Route path='/login' exact component={Login} />
+                <Route path='/register' exact component={Register} />
+                <Route path='/register/complete' exact component={RegisterComplete} />
+            </Switch>
+        </>
+    );
 }
 
 export default App;
